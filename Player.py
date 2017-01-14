4db281bd5c1c5c3d01d9fc11f53cdb0945ea1599
Seiji Ujihira <sejimhp@gmail.com>
2017-01-14 22:33:06 +0900

    add player class

---
+++ Player.py
@@ -0,0 +1,14 @@
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import pygame
+
+class Player:
+    def __init__(self):
+        self.hp = 10
+        self.x = 0
+        self.y = 0
+
+    def update(self):
+
+    def draw(self):
